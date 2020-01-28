66457fe86f4a373a76baad1782409c3a619c44a3
Sharma Bilaval <sharmb9@mcmaster.ca>
2020-01-27 20:07:42 -0500

    Fixed rendering issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Header from './components/Header'
 import './App.css';
-import About from './components/About';
+
 
 class App extends Component{
   render(){
